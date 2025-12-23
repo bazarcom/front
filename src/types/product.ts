5960262e6d882e6f3ab3e9e6de563799fe081b3e
AtakishiAliyev <atakishieliyev@gmail.com>
2025-12-23 15:39:03 +0400

    add demo data and category

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,3 +1,5 @@
+import { Price } from './price';
+
 export type Product = {
   _id: string;
   name: string;
@@ -6,4 +8,5 @@ export type Product = {
   image_url: string;
   market_name: string;
   category: string;
+  prices?: Price[];
 };
